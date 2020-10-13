3cb2c9e34cd993cae4c85d6c420be97fec70bd6c
Micah Jones <maj407@gmail.com>
2020-10-12 22:33:32 -0400

    cleaning up and refactoring

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ new Vue ({
 
   el: '#gameboard',
 
-
   data: {
     pointer: true,
     gotime: false,
-    k: 0,
     boxNum: 0,
     button: "START",
     speed: 300,
@@ -24,49 +22,32 @@ new Vue ({
     showByIndex: null,
 
     boardArray: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
 
     theNextArray: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
   },
 
   methods: {
-    start: function () {
-      console.log("hello im here")
 
-    },
-
-    mouseOver: function(){
-           this.active = !this.active;
-       },
-
-    showByNumber: function(i){
-      if (this.showByIndex == 1){
-        return true;
-      }
-    },
-
-    renderBoard: function() {
-
-    },
     clickHandler: function() {
       this.gotime = true;
       this.toggleButton();
@@ -74,15 +55,10 @@ new Vue ({
     },
 
     toggleButton: function() {
-      // if (button == "START"){
-      //
-      // }
-
       this.button == "START" ? this.button = "STOP" : this.button = "START";
     },
 
     originalBoard: function() {
-    console.log('hi fadfsadfa');
       for (let i = 1; i <= 10; i++ ){
         var node = document.createElement("div");
         node.classList.add("gridStylingLine", "inlineit", "flex-center", "p-0",  "m-0");
@@ -91,79 +67,32 @@ new Vue ({
           var square = document.createElement("div");
           square.classList.add("gridStyling", "border", "inlineit", "pointer");
           node.appendChild(square);
-          square.id = this.k;
 
-
-          // square.onclick = function(this.id) {console.log(id
+          square.setAttribute('data-column', j-1);
+          square.setAttribute('data-row', i-1);
           let vue = this;
-            square.onclick = function() {
-              // console.log("edit board");
-              vue.pointer = false;
+          square.onclick = function() {
+            vue.pointer = false;
 
+            if(vue.boardArray[this.getAttribute('data-row')][this.getAttribute('data-column')] === 0){
+              this.classList.add("blackBackground");
+              vue.boardArray[this.getAttribute('data-row')][this.getAttribute('data-column')] = 1;
+            }else{
+              this.classList.remove("blackBackground");
+              vue.boardArray[this.getAttribute('data-row')][this.getAttribute('data-column')] = 0;
+            }
 
-              let numTwo = 0;
-              let numOne = 0;
-
-              if(!this.id[1]){
-                numOne = 0;
-                numTwo = this.id;
-              }else{
-                numTwo = this.id[1];
-                numOne = this.id[0]
-
-              }
-              console.log("num one", numOne, numTwo);
-
-
-
-                // console.log("edit board");
-                if(vue.boardArray[numOne][numTwo] == 0){
-                  console.log("in the spot yeah");
-                  this.classList.add("blackBackground");
-                  vue.boardArray[numOne][numTwo] = 1;
-                }else{
-                  this.classList.remove("blackBackground");
-                  vue.boardArray[numOne][numTwo] = 0;
-                }
-
-              console.log(vue.boardArray);
-
-
-
-
-
-            };
-          this.k++;
+          };
         }
       }
     },
 
-    editBoard: function(square){
-      console.log("edit board");
-      if (square == 1){
-        if(this.boardArray[0][0] == 0){
-        this.boardArray[0][0] = 1;
-      }else{
-        this.boardArray[0][0] = 0;
-      }
-      }
-      console.log(this.boardArray);
-    },
-
-
-
     createBoard: function() {
-
-      console.log(this.boardArray);
-
       if (this.button == "START"){
         return;
       }
 
-
-      // console.log("this is before the loop")
-      // console.log(this.boardArray)
-       this.ticks += 1
+      this.ticks += 1
 
       let neighbors = 0;
       for (let i = 0; i < this.boardArray[0].length; i++){
@@ -174,15 +103,14 @@ new Vue ({
                 neighbors += 1
               }
             }
-              if (this.boardArray[i][t] == 1){
-                neighbors += 1
-              }
+            if (this.boardArray[i][t] == 1){
+              neighbors += 1
+            }
             if(this.boardArray[i+1]){
               if (this.boardArray[i+1][t] == 1){
                 neighbors += 1
               }
             }
-
           }
           if (this.boardArray[i][j] === 1){
             neighbors -= 1;
@@ -197,23 +125,17 @@ new Vue ({
             if (neighbors > 3){
               this.theNextArray[i][j] = 0
             }
-        }else if (this.boardArray[i][j] === 0){
-          if (neighbors === 3){
-            this.theNextArray[i][j] = 1
-          }else{
-            this.theNextArray[i][j] = 0
+          }else if (this.boardArray[i][j] === 0){
+            if (neighbors === 3){
+              this.theNextArray[i][j] = 1
+            }else{
+              this.theNextArray[i][j] = 0
+            }
           }
-
-
-        }
-        // console.log("at location" + i + "-" + j + "this location has" + neighbors + "neighbors")
-        //
            neighbors = 0
 
           }
         }
-
-
 
         this.boardArray = this.theNextArray;
         this.theNextArray = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -226,17 +148,7 @@ new Vue ({
                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-        // console.log('the board array', this.boardArray);
 
-
-        // console.log(this.stopit);
-        // if (this.stopit == false){
-        //   this.stopit = true;
-        //   this.createBoard();
-        //
-        // }else{
-        //   console.log('stopped');
-        // }
         let vue = this;
         setTimeout(function(){
           if (vue.stopit == false){
@@ -245,12 +157,6 @@ new Vue ({
         },this.speed)
   },
 
-    showIt: function(i){
-        // console.log("hello im here")
-      if (i == 1){
-        this.showByIndex = 1;
-      }
-    },
 
     stop: function(){
       this.stopit = !this.stopit;
@@ -259,15 +165,13 @@ new Vue ({
 
   mounted: function(){
     this.originalBoard()
-
- },
+  },
 
   computed: {
-  classObject: function () {
-    this.boxNum++;
-    return this.boxNum;
-
+    classObject: function () {
+      this.boxNum++;
+      return this.boxNum;
+    }
   }
-}
 
 });
